b0888b5e9676d528a68af1fc4261f08300a831cc
DarioJeurissen <d.jeurissen@gmail.com>
2016-11-10 14:46:19 +0100

    d

---
+++ src/main/java/be/vdab/entities/Docent.java
@@ -0,0 +1,68 @@
+package be.vdab.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import be.vdab.enums.Geslacht;
+
+@Entity
+@Table(name = "docenten")
+public class Docent implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String voornaam;
+	private String familienaam;
+	private BigDecimal wedde;
+	private long rijksRegisterNr;
+	
+	@Enumerated(EnumType.STRING)
+	private Geslacht geslacht;
+	
+	public Docent(){
+		
+	}
+	
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getVoornaam() {
+		return voornaam;
+	}
+
+	public String getFamilienaam() {
+		return familienaam;
+	}
+
+	public BigDecimal getWedde() {
+		return wedde;
+	}
+
+	public long getRijksRegisterNr() {
+		return rijksRegisterNr;
+	}
+
+	public Geslacht getGeslacht() {
+		return geslacht;
+	}
+
+	// je maakt getters voor de private variabelen, behalve voor
+	// serialVersionUID
+	public String getNaam() {
+		return voornaam + ' ' + familienaam;
+	}
+}
---
+++ src/main/java/be/vdab/repositories/DocentRepository.java
@@ -0,0 +1,14 @@
+package be.vdab.repositories;
+
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+
+import be.vdab.entities.Docent;
+
+// enkele imports ...
+public class DocentRepository {
+	public Optional<Docent> read(long id, EntityManager entityManager) {
+		return Optional.ofNullable(entityManager.find(Docent.class, id));
+		}
+}
